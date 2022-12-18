268d9f5cddf3854eea7cad634429a5874c078d1d
elios11 <eliosferre3@hotmail.com>
2022-12-17 21:34:19 -0300

    Add random dice value generator and displaying

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
+import { useState } from "react"
 import Die from "./components/Die/Die"
 
 export default function App() {
+    function generateDice() {
+        let randomDiceArray = []
+        for (let i = 0; i < 10; i++) {
+            randomDiceArray.push(Math.ceil(Math.random() * 6))
+        }
+        return randomDiceArray;
+    }
+
+    const [dice, setDice] = useState(() => generateDice())
+    const diceElements = dice.map((die, index) => {
+        return <Die key={index} value={die} />
+    })
+
     return (
         <main>
             <div className="dice-container">
-                <Die value={1}/>
-                <Die value={2}/>
-                <Die value={3}/>
-                <Die value={4}/>
-                <Die value={5}/>
-                <Die value={6}/>
-                <Die value={1}/>
-                <Die value={2}/>
-                <Die value={3}/>
-                <Die value={4}/>
+                {diceElements}
             </div>
         </main>
     )
