cc4609877c0accad421b5e16a08861577b2e5255
elios11 <eliosferre3@hotmail.com>
2022-12-18 01:10:53 -0300

    Hold selected dice between rolls

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,62 @@ import { nanoid } from "nanoid"
 export default function App() {
     const [dice, setDice] = useState(() => generateDice())
 
+    function createNewDie() {
+        return {
+            id: nanoid(),
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false
+        }
+    }
+
     function generateDice() {
         let randomDiceArray = []
         for (let i = 0; i < 10; i++) {
-            randomDiceArray.push({
-                id: nanoid(),
-                value: Math.ceil(Math.random() * 6),
-                isHeld: false
-            })
+            randomDiceArray.push(createNewDie())
         }
         return randomDiceArray;
     }
 
-    function reRollDice() {
-        setDice(generateDice())
+    function rerollDice() {
+        setDice(prevDice => (
+            prevDice.map(die => {
+                return die.isHeld
+                    ? die
+                    : createNewDie()
+            })
+        ))
+    }
+
+    function holdDice(id) {
+        setDice(prevDice => (
+            prevDice.map(die => {
+                return die.id === id
+                    ? {...die, isHeld: !die.isHeld}
+                    : die
+            })
+        ))
     }
 
     const diceElements = dice.map(die => {
         return <Die key={die.id}
+                    id={die.id}
                     value={die.value}
+                    holdDice={holdDice}
                     isHeld={die.isHeld}
                 />
     })
 
     return (
         <main>
+            <h1 className="game-title">Tenzi</h1>
+            <p className="game-instructions">
+                Roll until all dice are the same. Click each die to freeze it at
+                its current value between rolls.
+            </p>
             <div className="dice-container">
                 {diceElements}
             </div>
-            <button type="button" className="roll-button" onClick={reRollDice}>
+            <button type="button" className="roll-button" onClick={rerollDice}>
                 Roll
             </button>
         </main>
