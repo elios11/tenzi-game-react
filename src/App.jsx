f24007c80c8ec4cbb925c290525445cfc15e2839
elios11 <eliosferre3@hotmail.com>
2022-12-17 21:58:15 -0300

    Add roll btn and change array for array of objects

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,25 @@ import { useState } from "react"
 import Die from "./components/Die/Die"
 
 export default function App() {
+    const [dice, setDice] = useState(() => generateDice())
+
     function generateDice() {
         let randomDiceArray = []
         for (let i = 0; i < 10; i++) {
-            randomDiceArray.push(Math.ceil(Math.random() * 6))
+            randomDiceArray.push({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false
+            })
         }
         return randomDiceArray;
     }
 
-    const [dice, setDice] = useState(() => generateDice())
+    function reRollDice() {
+        setDice(generateDice())
+    }
+
     const diceElements = dice.map((die, index) => {
-        return <Die key={index} value={die} />
+        return <Die key={index} value={die.value} />
     })
 
     return (
@@ -20,6 +28,9 @@ export default function App() {
             <div className="dice-container">
                 {diceElements}
             </div>
+            <button type="button" className="roll-button" onClick={reRollDice}>
+                Roll
+            </button>
         </main>
     )
 }
